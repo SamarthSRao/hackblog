30921ef016b0a9aa215d4c415ab7c0e2d59ee38a
SamarthSRao <Samarthz0901@gmail.com>
2026-04-12 18:34:12 +0530

    frontend changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,45 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import MainPage from './mainPage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
-import RegisterPage from './pages/RegisterPage';
-import './App.css';
-import ProfilePage from './pages/ProfilePage';
-import CommentPage from './pages/CommentsPage';
-
-import SubmitPage from './pages/SubmitPage';
+import RegisterPage from './pages/RegisterPage.jsx';
+import ProfilePage from './pages/ProfilePage.jsx';
+import CommentPage from './pages/CommentsPage.jsx';
+import AskPage from './pages/AskPage.jsx';
+import SubmitPage from './pages/SubmitPage.jsx';
 import RecentCommentsPage from './pages/RecentCommentsPage.jsx';
-
+import { AuthProvider } from './context/Authcontext.jsx';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          {/* Public Routes */}
-          <Route path="/" element={<MainPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          {/* Catch all - redirect to home */}
-          <Route path="/ask" element={<AskPage />} />
-          <Route path="/comments" element={<RecentCommentsPage />} />
-          <Route path="/show" element={<ShowPage />} />
-          <Route path="/jobs" element={<JobsPage />} />
-          <Route path="/submit" element={<SubmitPage />} />
-          <Route path="/user/:userId" element={<ProfilePage />} />
-          <Route path="/comments/:postId" element={<CommentPage />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <AuthProvider>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            {/* Public Routes */}
+            <Route path="/" element={<MainPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            
+            {/* Page Routes */}
+            <Route path="/ask" element={<AskPage />} />
+            <Route path="/comments" element={<RecentCommentsPage />} />
+            <Route path="/submit" element={<SubmitPage />} />
+            <Route path="/user/:userId" element={<ProfilePage />} />
+            <Route path="/comments/:postId" element={<CommentPage />} />
+
+            {/* Placeholder/Redirect for missing pages */}
+            <Route path="/show" element={<Navigate to="/" replace />} />
+            <Route path="/jobs" element={<Navigate to="/" replace />} />
+            
+            {/* Catch all - redirect to home */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </AuthProvider>
   );
 }
 
 export default App;
+
